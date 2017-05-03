75c3c1fab70386c0aac38be7ea153399f9be0240
pinae <jonny@pinae.net>
2017-05-03 12:00:22 +0200

    First optimizer infrastructure

---
+++ optimize.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, division, print_function
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from lake_map import get_lake_map
+
+
+def create_noise_map(dimensions):
+    return np.random.random(size=(1, dimensions[1], dimensions[0], 1)) * 256
+
+
+class HeightmapOptimizer:
+    def __init__(self, init_map):
+        self.heightmap = tf.Variable(initial_value=tf.constant(init_map, dtype=tf.float32) / 256, name="Heightmap")
+        self.loss = tf.reduce_sum(get_lake_map(self.heightmap))
+        self.learning_rate = 0.1
+        self.optimizer = tf.train.AdamOptimizer(self.learning_rate,
+                                                beta1=0.9, beta2=0.999, epsilon=1e-08).minimize(self.loss)
+        init_op = tf.global_variables_initializer()
+        self.sess = tf.Session()
+        self.sess.run(init_op)
+
+    def optimize_step(self):
+        _, heightmap = self.sess.run([self.optimizer, self.heightmap * 256], feed_dict={})
+        return heightmap
+
+
+if __name__ == "__main__":
+    initial_map = create_noise_map((640, 480))
+    optimizer = HeightmapOptimizer(initial_map)
+    for i in range(200):
+        height_map = optimizer.optimize_step()
+        x = height_map.reshape((initial_map.shape[1], initial_map.shape[2])).astype(np.uint8)
+        x = np.dstack((x, x, x))
+        im = Image.fromarray(x)
+        im.save("images/image" + str(i) + ".jpg", quality=95)

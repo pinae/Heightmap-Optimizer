567f3a4d46db07a96a4c319184ddffa082f45226
pinae <jonny@pinae.net>
2017-05-03 11:59:57 +0200

    expand dims

--- lake_map.py
+++ lake_map.py
@@ -9,10 +9,10 @@ def get_lake_map(heightmap):
                            [[[0, 0, 0, 1, 0, 0, 0, 0]], [[0, 0, 0, 0, 0, 0, 0, 0]], [[0, 0, 0, 0, 1, 0, 0, 0]]],
                            [[[0, 0, 0, 0, 0, 1, 0, 0]], [[0, 0, 0, 0, 0, 0, 1, 0]], [[0, 0, 0, 0, 0, 0, 0, 1]]]],
                           dtype=tf.float32)
-    cropped = tf.slice(heightmap, [0, 1, 1, 0], [1, tf.shape(heightmap)[1]-2, tf.shape(heightmap)[2]-2, 1])
     min_surrounding_pixel = tf.reduce_min(tf.nn.conv2d(heightmap, filters, strides=[1, 1, 1, 1], padding='VALID'),
                                           axis=3)
-    return tf.nn.relu(min_surrounding_pixel - cropped)
+    cropped = tf.slice(heightmap, [0, 1, 1, 0], [1, tf.shape(heightmap)[1] - 2, tf.shape(heightmap)[2] - 2, 1])
+    return tf.nn.relu(tf.expand_dims(min_surrounding_pixel, axis=3) - cropped)
 
 
 if __name__ == "__main__":
